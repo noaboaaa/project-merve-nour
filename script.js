61c3b94a656ab526e8ed99c5d542dc3da1fb214d
noaboaaa <noarafati@hotmail.com>
2023-04-27 11:58:48 +0200

    x

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+window.addEventListener("load", initApp);
+
+// gloabal variables 
+const endpoint = "https://projekt-merve-nour-default-rtdb.firebaseio.com/";
+
+let posts;
+
+//=============================initApp=============================//
