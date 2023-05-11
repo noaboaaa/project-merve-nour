5f04b419bd6829105a8207330db140e0a7a92a2f
noaboaaa <noarafati@hotmail.com>
2023-05-11 16:28:03 +0200

    1

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ window.addEventListener("load", initApp);
 function initApp() {
   console.log("så dæh nu dåh");
   updatePostsGrid();
-  //--------------CREATEPOST-----------------//
 
+  //--------------CREATEPOST------------------------//
   document
     .querySelector("#open-create-post-dialog")
     .addEventListener("click", showCreatePost);
@@ -24,7 +24,7 @@ function initApp() {
     .querySelector("#form-create-post")
     .addEventListener("submit", createPostClicked);
 
-  //-------------UPDATE POST/ DIALOG CLICKED-------------//
+  //-------------UPDATE POST/ DIALOG CLICKED---------//
   document
     .querySelector("#btn-open-update-post-dialog")
     .addEventListener("click", showUpdatePost);
@@ -37,14 +37,12 @@ function initApp() {
     .querySelector("#form-update-post")
     .addEventListener("submit", updatePostClicked);
 
-
-  //-------------SEARCH SORT FILTER-------------//
-
+  //-------------SEARCH SORT FILTER------------------//
   document
     .querySelector("#input-search")
     .addEventListener("input", searchPosts);
 
-  //-------------SORT POSTS-------------//
+  //-------------SORT POSTS--------------------------//
   document.querySelector("#sort-options").addEventListener("change", sortPosts);
 }
 
@@ -71,37 +69,34 @@ async function getPosts() {
   }));
   return postObjects;
 }
-
-// Generate HTML for each post and append it to the #posts element
 function showPosts(listOfPosts) {
   const postsContainer = document.querySelector("#posts");
   postsContainer.innerHTML = "";
 
   for (const post of listOfPosts) {
-    const html = /*html*/ `
-      <article class="grid-item">
-        <img src="${post.image}" data-id="${post.id}" />
-      </article>
-    `;
+    const article = document.createElement("article");
+    article.classList.add("grid-item");
 
-    postsContainer.insertAdjacentHTML("beforeend", html);
+    const img = document.createElement("img");
+    img.src = post.image;
+    img.setAttribute("data-id", post.id);
+
+    article.appendChild(img);
+    postsContainer.appendChild(article);
   }
 
-  // Add a click event listener to the posts container
   postsContainer.addEventListener("click", function (event) {
-    // Check if an image was clicked
     if (event.target.tagName === "IMG") {
-      // Find the clicked post by its id
       const postId = event.target.dataset.id;
       const post = listOfPosts.find((post) => post.id === postId);
 
-      // If a post was found, show its details
       if (post) {
         imageClicked(post);
       }
     }
   });
 }
+
 
 //===================SEARCH SORT FILTER===========================//
 
@@ -121,17 +116,10 @@ function sortPosts(event) {
   const option = event.target.value;
 
   if (option === "year") {
-    // Sort by year
     posts.sort((a, b) => b.year - a.year);
   } else if (option === "song") {
-    // Sort by song
     posts.sort((a, b) => a.song.localeCompare(b.song));
-  } else if (option === "nosort") {
-    // Sort by id to get original order (assuming ids are incremental and reflect the original order)
-    posts.sort((a, b) => a.id - b.id);
   }
-
-  // Update display
   showPosts(posts);
 }
 
@@ -143,7 +131,6 @@ function showCreatePost() {
   if (createPostDialog.showModal) {
     createPostDialog.showModal();
   } else {
-    // In case the browser does not support the `showModal` method, fall back to displaying the dialog using CSS
     createPostDialog.style.display = "block";
     document.querySelector("#overlay").style.display = "block";
   }
@@ -151,18 +138,22 @@ function showCreatePost() {
 
 function hideCreatePost() {
   console.log("closed create post dialog!");
-  const createPostDialog = document.querySelector("#close-create-button");
-  if (createPostDialog.close) {
-    createPostDialog.close();
+  const closeCreateButton = document.querySelector("#close-create-post-dialog");
+  const createPostDialog = document.querySelector("#create-post-dialog");
+  if (closeCreateButton && createPostDialog) {
+    if (createPostDialog.close) {
+      createPostDialog.close();
+    } else {
+      createPostDialog.style.display = "none";
+      document.querySelector("#overlay").style.display = "none";
+    }
   } else {
-    // In case the browser does not support the `close` method, fall back to hiding the dialog using CSS
-    createPostDialog.style.display = "none";
-    document.querySelector("#overlay").style.display = "none";
+    console.error("The button or dialog element is not found in the HTML.");
   }
 }
 
 function createPostClicked(event) {
-  event.preventDefault(); // Prevent form submission and page refresh
+  event.preventDefault(); 
   const form = this;
   console.log("in form");
   console.log(form);
@@ -174,7 +165,6 @@ function createPostClicked(event) {
   createPost(image, description, name, year, song);
   form.reset();
   document.querySelector("#create-post-dialog").close();
-
 }
 
 async function createPost(image, description, name, year, song) {
@@ -187,13 +177,32 @@ async function createPost(image, description, name, year, song) {
   };
   const json = JSON.stringify(newPost);
   console.log(json);
+
   const response = await fetch(`${endpoint}/posts.json`, {
     method: "POST",
     body: json,
   });
+
   if (response.ok) {
-    console.log("Data added to FireBase!");
+    console.log("Data added to Firebase!");
+
+    // Show success pop-up and hide it after 3 seconds
+    const successPopup = document.querySelector("#create-success-popup");
+    successPopup.style.display = "block";
+    setTimeout(() => {
+      successPopup.style.display = "none";
+    }, 3000);
+
     updatePostsGrid();
+  } else {
+    console.error("Error posting the post.");
+
+    // Show error pop-up and hide it after 3 seconds
+    const errorPopup = document.querySelector("#create-error-popup");
+    errorPopup.style.display = "block";
+    setTimeout(() => {
+      errorPopup.style.display = "none";
+    }, 3000);
   }
 }
 
@@ -206,61 +215,62 @@ async function updatePostsGrid() {
 
 // called when image is clicked
 function imageClicked(postObject) {
-    console.log(postObject); // Debugging statement
+  console.log(postObject);
 
-    document.querySelector("#dialog-image").src = postObject.image;
+  document.querySelector("#dialog-image").src = postObject.image;
+  document.querySelector("#name").textContent = postObject.name;
+  document.querySelector("#year").textContent = postObject.year;
+  document.querySelector("#song").textContent = postObject.song;
+  document.querySelector("#Description").textContent = postObject.description;
 
-    document.querySelector("#name").textContent = postObject.name;
-    document.querySelector("#year").textContent = postObject.year;
-    document.querySelector("#song").textContent = postObject.song;
-    document.querySelector("#Description").textContent = postObject.description;
+  // Add event listeners for delete, update and close buttons in the detail view
+  document
+    .querySelector("#btn-delete")
+    .addEventListener("click", () => deleteClicked(postObject));
+  document
+    .querySelector("#btn-open-update-post-dialog")
+    .addEventListener("click", () => showUpdatePost(postObject));
+  document
+    .querySelector("#btn-close-dialog")
+    .addEventListener("click", hidePostClicked);
 
-    // Add event listeners for delete and update buttons in the detail view
-    document.querySelector("#btn-delete").addEventListener("click", () => deleteClicked(postObject));
-    document.querySelector("#btn-open-update-post-dialog").addEventListener("click", () => showUpdatePost(postObject));
-
-    // Open the dialog
-    document.querySelector("#post-clicked-dialog").showModal();
+  document.querySelector("#post-clicked-dialog").showModal();
 }
 
 //-------closes dialog when "close" is clicked -----
 function hidePostClicked() {
-    console.log("closed post clicked dialog!");
-    const postClickedDialog = document.querySelector("#post-clicked-dialog");
-    if (postClickedDialog.close) {
-        postClickedDialog.close();
-    } else {
-        postClickedDialog.style.display = "none";
-        document.querySelector("#overlay").style.display = "none";
-    }
+  console.log("closed post clicked dialog!");
+  const postClickedDialog = document.querySelector("#post-clicked-dialog");
+  if (postClickedDialog.close) {
+    postClickedDialog.close();
+  } else {
+    postClickedDialog.style.display = "none";
+    document.querySelector("#overlay").style.display = "none";
+  }
 }
 
 //-------pop up, deletes post, closes dialog and updates grid---
 async function deleteClicked(postObject) {
-    // Ask the user to confirm the deletion
-    const confirmed = confirm("Are you sure you want to delete this post?");
+  const confirmed = confirm("Are you sure you want to delete this post?");
 
-    // If the user confirmed the deletion
-    if (confirmed) {
-        const postId = postObject.id;
-        const response = await fetch(`${endpoint}/posts/${postId}.json`, {
-            method: "DELETE"
-        });
+  if (confirmed) {
+    const postId = postObject.id;
+    const response = await fetch(`${endpoint}/posts/${postId}.json`, {
+      method: "DELETE",
+    });
 
-        if (response.ok) {
-            alert("Post successfully deleted!");
-            updatePostsGrid();
+    if (response.ok) {
+      alert("Post successfully deleted!");
+      updatePostsGrid();
 
-            // Close the dialog after post is deleted
-            const postClickedDialog = document.querySelector("#post-clicked-dialog");
-            postClickedDialog.close();
-        } else {
-            alert("Error deleting the post. Please try again.");
-        }
+      const postClickedDialog = document.querySelector("#post-clicked-dialog");
+      postClickedDialog.close();
+    } else {
+      alert("Error deleting the post. Please try again.");
     }
+  }
 }
 
-//======================UPDATE POST =========
 
 //======================UPDATE POST ======================//
 
@@ -275,16 +285,21 @@ function showUpdatePost(postObject) {
   }
 
   console.log("opened update post dialog!");
-  const updatePostDialog = document.querySelector("#update-post-dialog");
 
+  const updatePostDialog = document.querySelector("#update-post-dialog");
   const updateForm = document.querySelector("#form-update-post");
 
   // Get postId from the postObject
   let postId = postObject.id;
 
-  // Set the postId as a data attribute on the form
+  // Prefill the form with the current post information
   if (updateForm) {
     updateForm.setAttribute("data-post-id", postId);
+    updateForm.image.value = postObject.image;
+    updateForm.description.value = postObject.description;
+    updateForm.name.value = postObject.name;
+    updateForm.year.value = postObject.year;
+    updateForm.song.value = postObject.song;
   } else {
     console.error("The #form-update-post element is not found in the HTML.");
   }
@@ -298,8 +313,6 @@ function showUpdatePost(postObject) {
     }
   }
 }
-
-
 
 function hideUpdatePost() {
   console.log("closed update post dialog!");
@@ -323,7 +336,7 @@ function updatePostClicked(event) {
   const year = form.year.value;
   const song = form.song.value;
 
-  const postId = form.getAttribute("data-post-id"); // Get the postId from the form's data attribute
+  const postId = form.getAttribute("data-post-id"); 
 
   updatePost(postId, image, description, name, year, song);
   form.reset();
@@ -345,14 +358,32 @@ async function updatePost(postId, image, description, name, year, song) {
     method: "PATCH", // Use PATCH method to update the existing post
     body: json,
   });
+
   if (response.ok) {
     console.log("Data updated on FireBase!");
+
+    // Show success pop-up and hide it after 3 seconds
+    const successPopup = document.querySelector("#success-popup");
+    successPopup.style.display = "block";
+    setTimeout(() => {
+      successPopup.style.display = "none";
+    }, 3000);
+
     updatePostsGrid();
+  } else {
+    console.log("Error updating the post.");
+
+    // Show error pop-up and hide it after 3 seconds
+    const errorPopup = document.querySelector("#error-popup");
+    errorPopup.style.display = "block";
+    setTimeout(() => {
+      errorPopup.style.display = "none";
+    }, 3000);
   }
 }
 
 async function updatePostsGrid() {
   posts = await getPosts();
+  posts.sort((a, b) => b.id.localeCompare(a.id)); // Sort by id (newest first)
   showPosts(posts);
 }
-
